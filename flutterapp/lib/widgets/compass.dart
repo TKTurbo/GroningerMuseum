aaad944d24bdc49be8c1a82a1709f50fa9da6eae
Thomas <t.kreder@st.hanze.nl>
2022-04-19 20:37:18 +0200

    Restructured route screen and added compass to widgets

---
+++ flutterapp/lib/widgets/compass.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'package:fluttermockup/screens/routes_screen.dart';
+import 'dart:math' as math;
+
+class Compass extends StatelessWidget {
+  static const locationDirection = 0; // Direction the user should move to
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   // TODO: implement build
+  //   throw UnimplementedError();
+  // }
+
+  Widget build(BuildContext context) {
+    return StreamBuilder<CompassEvent>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error reading heading: ${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        double? direction = snapshot.data!.heading;
+
+        // if direction is null, then device does not support this sensor
+        // show error message
+        if (direction == null) {
+          return const Center(
+            child: Text("Device does not have sensors !"),
+          );
+        }
+
+        return Material(
+          shape: CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 16.0,
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: getDirectionColorFeedback(locationDirection, direction),
+            ),
+            child: Transform.rotate(
+              angle: (direction * (math.pi / 180) * -1),
+              child: const Icon(
+                Icons.arrow_upward,
+                color: Colors.black,
+                size: 264.0,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  getDirectionColorFeedback(locationAt, pointingAt) {
+    // Gets arrow color depending on how close the user is to the right direction
+    // locationAt is the angle from point a to point b
+    // pointingAt is the current direction the user is pointing at
+
+    var diff = (locationAt + pointingAt).abs(); // absolute difference between the location angle and the pointing angle
+    // debugPrint('locationAt: ' + locationAt.toString());
+    // debugPrint('pointingAt: ' + pointingAt.toString());
+    // debugPrint('diff: ' + diff.toString());
+
+    if (diff < 10) {
+      // green
+      return Colors.green;
+    } else if(diff < 20) {
+      // orange
+      return Colors.orange;
+    } else {
+      // red
+      return Colors.red;
+    }
+
+  }
+}
