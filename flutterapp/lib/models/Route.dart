93c63be988c11577a4931d348c34712baecd3bc2
TKTurbo <t.kreder@st.hanze.nl>
2022-04-26 11:10:07 +0200

    Small changes

--- flutterapp/lib/models/Route.dart
+++ flutterapp/lib/models/Route.dart
@@ -2,6 +2,4 @@ class Route {
   final String id;
   final String description;
   final String JSON;
-
-  
 }
--- flutterapp/lib/screens/route_screen.dart
+++ flutterapp/lib/screens/route_screen.dart
@@ -11,20 +11,11 @@ class RouteScreen extends StatelessWidget {
 
   static const locationDirection = 0; // Direction the user should move to
 
+  // Fetch the route from the API
   fetchRoute() async {
-    var url =
-    Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
-
-    // Await the http get response, then decode the json-formatted response.
+    var url = Uri.https('http://localhost:8080/', '/', {'q': '{http}'});
     var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse =
-      convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['totalItems'];
-      print('Number of books about http: $itemCount.');
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
+    print(response.body);
   }
 
   @override
