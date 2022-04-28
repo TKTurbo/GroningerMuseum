9791388770d4ab17296825d71893db7915921c75
Thomas <t.kreder@st.hanze.nl>
2022-04-29 01:13:20 +0200

    Route page details

--- flutterapp/lib/screens/route_screen.dart
+++ flutterapp/lib/screens/route_screen.dart
@@ -6,11 +6,13 @@ import 'dart:convert' as convert;
 
 import '../widgets/compass.dart';
 
+// TODO: should be stateful
 class RouteScreen extends StatelessWidget {
   // RouteScreen({Key? key}) : super(key: key);
 
   static const locationDirection = 0; // Direction the user should move to
   var route;
+  var selectedIndex = 0;
 
   Future<http.Response> fetchRoute() async {
     return http.get(Uri.parse('http://192.168.178.64:8080/'));
@@ -23,28 +25,54 @@ class RouteScreen extends StatelessWidget {
     });
   }
 
+  void onItemTapped(int index) {
+    if (index == 0 && selectedIndex > 0) {
+      selectedIndex--;
+    } else if (index == 1 && selectedIndex < route['path'].length) {
+      selectedIndex++;
+    }
+    print(selectedIndex);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-
-    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments; // Get screen args
+    final args = ModalRoute.of(context)!.settings.arguments
+        as ScreenArguments; // Get screen args
 
     return Scaffold(
       appBar: AppBar(
-        // title: Text('Route: ' + 'dsasddsa'),
-        title: FutureBuilder(
-          future: route,
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            print(route);
-            if(route == null) {
-              return Text('Route aan het laden...');
-            } else {
-              return Text('Route: ' + route['meta']['route_name']);
-            }
-          },
-        )
-      ),
-      body: Center(
-        child: Compass()
+          // title: Text('Route: ' + 'dsasddsa'),
+          title: FutureBuilder(
+        future: route,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          print(route);
+          if (route == null) {
+            return Text('Route aan het laden...');
+          } else {
+            return Text('Route: ' + route['meta']['route_name'] + ' | Lengte: ' + route['path'].length.toString());
+          }
+        },
+      )),
+      body: Center(child: Compass()),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_back),
+            label: 'Vorige',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_forward),
+            label: 'Volgende',
+            backgroundColor: Colors.blue,
+          ),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.green[800],
+        // TODO: dirty fix
+        unselectedItemColor: Colors.green[800],
+        onTap: onItemTapped,
       ),
     );
   }
