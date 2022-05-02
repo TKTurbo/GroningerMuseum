5047e56e6eec7384c3f6c38a4acc265dd0d52815
Thomas <t.kreder@st.hanze.nl>
2022-05-02 21:48:08 +0200

    Gekke future error weg door http call te commenten

--- flutterapp/lib/screens/route_screen.dart
+++ flutterapp/lib/screens/route_screen.dart
@@ -9,47 +9,47 @@ import '../widgets/compass.dart';
 // TODO: should be stateful
 class RouteScreen extends StatefulWidget {
   var route;
-  var selectedIndex = 0;
 
-  Future<http.Response> fetchRoute() async {
-    return http.get(Uri.parse('http://192.168.178.64:8080/'));
-  }
-
-  RouteScreen({Key? key}) : super(key: key) {
-// works somehow? TODO: make cleaner when errors
-    route = fetchRoute().then((resp) {
-      route = convert.jsonDecode(resp.body);
-    });
-  }
+//   Future<http.Response> fetchRoute() async {
+//     return http.get(Uri.parse('http://192.168.178.64:8080/'));
+//   }
+//
+//   RouteScreen({Key? key}) : super(key: key) {
+// // works somehow? TODO: make cleaner when errors
+//     route = fetchRoute().then((resp) {
+//       route = convert.jsonDecode(resp.body);
+//     });
+//   }
 
   @override
-  State<RouteScreen> createState() => RouteScreenState();
+  RouteScreenState createState() => RouteScreenState();
 }
 
 class RouteScreenState extends State<RouteScreen> {
   static const locationDirection = 0; // Direction the user should move to
-  var selectedIndex = 0;
+  int selectedIndex = 0;
   var hoi = 'moi';
 
-  void onItemTapped(int index) {
-    if (index == 0 && selectedIndex > 0) {
-      // widget.selectedIndex--;
-      // setState(() {
-      //   selectedIndex--;
-      // });
-    } else if (index == 2 && selectedIndex < widget.route['path'].length) {
-      // widget.selectedIndex++;
-      setState(() {
-        selectedIndex = 400;
-      });
-    }
-    print('adssaddsadsa');
-    print(selectedIndex);
+  void onItemTapped(int index) async {
+    setState(() {
+      selectedIndex = index;
+    });
+  //   print(index);
+  //   if (index == 0 && selectedIndex > 0) {
+  //     // setState(() {
+  //     //   selectedIndex--;
+  //     // });
+  //   } else if (index == 2 && selectedIndex < widget.route['path'].length) {
+  //     // widget.selectedIndex++;
+  //       selectedIndex = 400;
+  //   }
+    // print('adssaddsadsa');
+    // print(selectedIndex);
+    // setState(() => selectedIndex = 4002);
   }
 
   @override
   Widget build(BuildContext context) {
-    selectedIndex = widget.selectedIndex;
     final args = ModalRoute.of(context)!.settings.arguments
         as ScreenArguments; // Get screen args
 
@@ -59,37 +59,37 @@ class RouteScreenState extends State<RouteScreen> {
           title: FutureBuilder(
         future: widget.route,
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          print(widget.route);
           if (widget.route == null) {
             return Text('Route aan het laden...');
           } else {
-            return Text('Route: ' +
-                widget.route['meta']['route_name'] +
-                ' | Lengte: ' +
-                widget.route['path'].length.toString());
+            // return Text('Route: ' +
+            //     widget.route['meta']['route_name'] +
+            //     ' | Lengte: ' +
+            //     widget.route['path'].length.toString());
+            return Text('moi');
           }
         },
       )),
       body: Center(child: Compass()),
       bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
             icon: Icon(Icons.arrow_back),
             label: 'Vorige',
             backgroundColor: Colors.green,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.arrow_downward),
-            label: widget.selectedIndex.toString(),
+            label: 'Index',
             backgroundColor: Colors.black,
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.arrow_forward),
             label: 'Volgende',
             backgroundColor: Colors.blue,
           ),
         ],
-        currentIndex: widget.selectedIndex,
+        currentIndex: selectedIndex,
         selectedItemColor: Colors.green[800],
         // TODO: dirty fix
         unselectedItemColor: Colors.green[800],
