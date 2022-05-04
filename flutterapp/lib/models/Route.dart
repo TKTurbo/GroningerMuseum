b165b9d1bf8d2df90f52569b048451621cac5dc8
Thomas <t.kreder@st.hanze.nl>
2022-05-04 20:04:51 +0200

    Routescreen werkt nu met http call en statelesswidget zonder te crashen

--- flutterapp/lib/models/Route.dart
+++ flutterapp/lib/models/Route.dart
@@ -1,5 +1,16 @@
 class Route {
-  final String id;
-  final String description;
-  final String JSON;
+  final String name;
+  final path;
+
+  const Route({
+    required this.name,
+    required this.path,
+  });
+
+  factory Route.fromJson(Map<String, dynamic> json) {
+    return Route(
+      name: json['meta']['route_name'],
+      path: json['path'],
+    );
+  }
 }
--- flutterapp/lib/screens/route_screen.dart
+++ flutterapp/lib/screens/route_screen.dart
@@ -1,26 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttermockup/screens/routes_screen.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'package:fluttermockup/models/Route.dart' as Route;
 
 import '../widgets/compass.dart';
 
 // TODO: should be stateful
 class RouteScreen extends StatefulWidget {
-  var route;
-
-//   Future<http.Response> fetchRoute() async {
-//     return http.get(Uri.parse('http://192.168.178.64:8080/'));
-//   }
-//
-//   RouteScreen({Key? key}) : super(key: key) {
-// // works somehow? TODO: make cleaner when errors
-//     route = fetchRoute().then((resp) {
-//       route = convert.jsonDecode(resp.body);
-//     });
-//   }
-
   @override
   RouteScreenState createState() => RouteScreenState();
 }
@@ -29,21 +19,58 @@ class RouteScreenState extends State<RouteScreen> {
   static const locationDirection = 0; // Direction the user should move to
   int selectedIndex = 0;
   var hoi = 'moi';
+  var route;
+  late Future futureRoute;
+
+  @override
+  void initState() {
+    super.initState();
+    futureRoute = fetchRoute();
+  }
+
+  // RouteScreenState() {
+  //   route = fetchRoute().then((resp) {
+  //     route = convert.jsonDecode(resp.body);
+  //   });
+  //
+  //   print(32987231987321321);
+  //
+  //   print(route);
+  // }
+
+  Future<http.Response> fetchRoute() async {
+    // final response = await http.get(Uri.parse('http://192.168.178.64:8080'));
+    //
+    // if (response.statusCode == 200) {
+    //   // If the server did return a 200 OK response,
+    //   // then parse the JSON.
+    //   return Route.fromJson(jsonDecode(response.body));
+    // } else {
+    //   // If the server did not return a 200 OK response,
+    //   // then throw an exception.
+    //   throw Exception('Failed to load album');
+    // }
+    return http.get(Uri.parse('http://192.168.178.64:8080/'));
+  }
+
+//   RouteScreen({Key? key}) : super(key: key) {
+// // works somehow? TODO: make cleaner when errors
+//     route = fetchRoute().then((resp) {
+//       route = convert.jsonDecode(resp.body);
+//     });
+//   }
 
   void onItemTapped(int index) {
     setState(() {
-      selectedIndex++;
+      if (index == 0 && selectedIndex > 0) {
+        selectedIndex--;
+      } else if (index == 2 && selectedIndex < 6) {
+        selectedIndex++;
+      } else if (index == 1) {
+        // TODO: explain selected index in route
+        print(selectedIndex);
+      }
     });
-    // print(index);
-    // setState(() {
-    //   if (index == 0 && selectedIndex > 0) {
-    //     selectedIndex--;
-    //   } else if (index == 2 && selectedIndex < 6) {
-    //     selectedIndex++;
-    //   } else {
-    //     print(selectedIndex);
-    //   }
-    // });
   }
 
   @override
@@ -55,16 +82,17 @@ class RouteScreenState extends State<RouteScreen> {
       appBar: AppBar(
           // title: Text('Route: ' + 'dsasddsa'),
           title: FutureBuilder(
-        future: widget.route,
+        future: futureRoute,
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (widget.route == null) {
-            return Text('Route aan het laden...');
+          if (snapshot.hasData) {
+            var route = json.decode(snapshot.data.body);
+            return Text('Route: ' +
+                route['meta']['route_name'] +
+                ' | Lengte: ' + selectedIndex.toString() + '/' + route['path'].length.toString());
+          } else if(snapshot.hasError) {
+            return Text('${snapshot.error}');
           } else {
-            // return Text('Route: ' +
-            //     widget.route['meta']['route_name'] +
-            //     ' | Lengte: ' +
-            //     widget.route['path'].length.toString());
-            return Text('moi');
+            return Text('Route aan het laden...');
           }
         },
       )),
@@ -87,7 +115,7 @@ class RouteScreenState extends State<RouteScreen> {
             backgroundColor: Colors.blue,
           ),
         ],
-        currentIndex: selectedIndex,
+        currentIndex: 0,
         selectedItemColor: Colors.green[800],
         // TODO: dirty fix
         unselectedItemColor: Colors.green[800],
